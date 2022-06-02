a0c2c6ece1734d08f2af772a9083575bdd797aa1
iremguvendik <iremguvendik@gmail.com>
2022-06-02 12:03:51 +0300

    2nd commit

--- src/gitreview/gitReview01.java
+++ src/gitreview/gitReview01.java
@@ -4,6 +4,8 @@ public class gitReview01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("ý learned git");
+		System.out.println("I am ok");
 
 	}
 
